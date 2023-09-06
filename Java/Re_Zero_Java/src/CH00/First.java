2623d60911d57a0c03d957cf6013e9cc42339457
gi-dor <rltjs3563@naver.com>
2023-09-06 10:57:28 +0900

    Rename :  폴더 위치 변경

---
+++ Java/Re_Zero_Java/src/CH00/First.java
@@ -0,0 +1,10 @@
+package CH00;
+
+public class First {
+    public static void main(String[] args) {
+        int Test = 1;
+        System.out.println("Test = " + Test);
+        System.out.println("args = " + args);
+
+    }
+}
