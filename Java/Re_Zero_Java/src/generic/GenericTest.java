8eb398a36087fdbf0ace891eeace486a2c4277f4
gi-dor <rltjs3563@naver.com>
2023-12-12 19:44:44 +0900

    Feat : 제네릭스 개념 / 자바의정석 인강

--- Java/Re_Zero_Java/src/generic/GenericTest.java
+++ Java/Re_Zero_Java/src/generic/GenericTest.java
@@ -6,18 +6,26 @@ public class GenericTest {
 
     public static void main(String[] args) {
 
+        /**
+         * 제네릭스 왜씀 ?
+         * 컴파일러에게 타입정보를 알려줘서 실행전에 타입체크가 강화되서 타입체크 잘해줘!
+         * 형변환 또 해주기 귀찮아!
+         * 코드도 간결하게 해줘!
+         * JDK1.5 이후부터 사용이가능해
+         */
+
 //      ArrayList list = new ArrayList();
         ArrayList<Integer> list = new ArrayList<Integer>();
-        list.add(30); // Integer OK
-        list.add(290); // list.add(new Integer(290);  에서 AUTO BOSING으로 list.add(290)
-//        list.add("44");     // String
-        list.add(44);   // 제네릭스 때문에 타입 체크가 강화됨.
+        list.add(30);       // Integer OK
+        list.add(290);      // list.add(new Integer(290);  에서 AUTO BOXING으로 list.add(290)
+//      list.add("44");     // String
+        list.add(44);       // 제네릭스 때문에 타입 체크가 강화됨.
         System.out.println(list);   // [30, 290, 44]
 
         //  Integer i = list.get(2); // 형변환 문제
         Integer i = (Integer) list.get(2);  // 컴파일은 OK - 컴파일러가 체크
         System.out.println(i);
-        System.out.println(list); // ClassCastException - 실행시 발생하는 형변환 에러
+        System.out.println(list); // ClassCastException -> RuntimeException 실행시 발생하는 형변환 에러 프로그래머 실수로 발생하는 에러
         // 실행시 발생하는 형변환 에러를 어떻게 처리할까 -> 제네릭스
 
         // Integer 말고 여러종류의 객체를 받고 싶어!
