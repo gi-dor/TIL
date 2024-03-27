cc5d57d20f1fb0df51c7078e4c21dfb61fc5f120
gi-dor <rltjs3563@naver.com>
2024-03-27 20:16:27 +0900

    Feat : 변수

---
+++ Java/Re_Zero_Java/src/variable/Var1.java
@@ -0,0 +1,24 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+
+        /*
+        변수  x ,  y   , z
+         */
+        int x = 10;
+        int y = 20;
+        int z = 30;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+
+        /*
+        변수  x ,  y   , z
+         */
+    }
+}
