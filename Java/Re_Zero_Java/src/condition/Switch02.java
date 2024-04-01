4350ff900f41514addca3dd5cb661936152c6bfd
gi-dor <rltjs3563@naver.com>
2024-04-01 18:39:59 +0900

    Feat : break 가 없는 Switch문

---
+++ Java/Re_Zero_Java/src/condition/Switch02.java
@@ -0,0 +1,25 @@
+package condition;
+
+public class Switch02 {
+
+    public static void main(String[] args) {
+
+        int grade = 2;
+        int coupon;
+
+        switch (grade) {
+            case 1:
+                coupon = 1000;
+//                System.out.println("case1 ,  coupon 1000");
+                break;
+            case 2:
+            case 3:
+                coupon = 3000;
+                break;
+            default:
+                coupon = 500;
+                break;
+        }
+        System.out.println("발급 받은 쿠폰 = " + coupon);
+    }
+}
