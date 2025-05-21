49dff6c96b66dafa7c8bc19207c5dc81af762e99
gi-dor <rltjs3563@naver.com>
2025-05-21 23:27:53 +0900

    Feat : 배열

--- JS/beginner/src/arrayFunction01.js
+++ JS/beginner/src/arrayFunction01.js
@@ -2,3 +2,8 @@ let arr = [1, 2, 3, 4, 5];
 for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
 }
+console.log('------------------------------')
+
+arr.forEach((element) => {
+    console.log(element)
+});
