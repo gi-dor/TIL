84d5ce75c6df2d569244f893c9210ba2a265840b
gi-dor <rltjs3563@naver.com>
2026-01-31 22:10:09 +0900

    초기화

---
+++ Java/Re_Zero_Java/src/type/InitData.java
@@ -0,0 +1,6 @@
+package type;
+
+public class InitData {
+    int value1; // 초기화 x
+    int value2 = 11; // 11 초기화
+}
---
+++ Java/Re_Zero_Java/src/type/InitMain.java
@@ -0,0 +1,9 @@
+package type;
+
+public class InitMain {
+    public static void main(String[] args) {
+        InitData data = new InitData();
+        System.out.println("value1 초기화x = " + data.value1);
+        System.out.println("value2 초기화O = " + data.value2);
+    }
+}
