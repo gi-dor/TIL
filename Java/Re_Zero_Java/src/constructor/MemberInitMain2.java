f44dba51bbeefa9706a58f9b1e69d256d0ef4cba
gi-dor <rltjs3563@naver.com>
2026-02-28 19:49:39 +0900

    생성자

--- Java/Re_Zero_Java/src/constructor/MemberInitMain2.java
+++ Java/Re_Zero_Java/src/constructor/MemberInitMain2.java
@@ -1,11 +1,11 @@
 package constructor;
 
-public class MemberInitMain1 {
+public class MemberInitMain2 {
 
     String nameField;
 
     // nameField 앞에 this가 없어도 멤버변수에 접근한다
-    void initMember(String nameParameter) {
+    void initMember2(String nameParameter) {
         nameField = nameParameter;
         // nameParameter는 매개변수에서 같은 이름이 있는지 찾음 ,매개변수가 있으므로 매개변수를 사용한다
         // nameField 먼저 지역변수(매개변수)에서 같은 이름이 있는지 찾는다 , 없으므로 멤버변수에서 찾음
@@ -19,9 +19,10 @@ public class MemberInitMain1 {
 
     public static void main(String[] args) {
         MemberInit member1 = new MemberInit();
-        member1.name = "user1";
-        member1.age = 15;
-        member1.grade = 90;
+        initMember(member1 , "user3" , 14 , 90);
+        //        member1.name = "user1";
+    //        member1.age = 15;
+    //        member1.grade = 90;
 
         MemberInit member2 = new MemberInit();
         member2.name = "user2";
@@ -33,4 +34,12 @@ public class MemberInitMain1 {
             System.out.println("이름 : " + member.name + " 나이 : " + member.age + " 성적 : " + member.grade);
         }
     }
+
+    static void initMember(MemberInit member , String name , int age , int grade) {
+        member.name = name;
+        member.age = age;
+        member.grade = grade;
+    }
+
+
 }
